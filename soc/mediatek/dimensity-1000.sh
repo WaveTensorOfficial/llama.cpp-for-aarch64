5cdcbf53c54a59cd5025e8f8dbe2234a623fa7ef
Jetson Tan <1809733265@qq.com>
2026-02-24 19:41:23 +0800

    Add files via upload

---
+++ soc/mediatek/dimensity-1000.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# By https://github.com/Insecta258 Email:2701262643@qq.com
+
+pkg update
+pkg upgrade
+
+echo "Checking environment..."
+pkg install ninja clang git make
+
+echo "现版本llama.cpp不支持Mali-G77的Vulkan驱动, 回退到CPU"
+
+sleep 1
+
+# 清理构建目录
+mkdir build && cd build
+
+# 修复后的编译器标志 - 移除冲突选项
+cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_CXX_FLAGS="-O3 -flto -march=armv8.2-a+dotprod -mtune=cortex-a77 -DNDEBUG" \
+  -DCMAKE_C_FLAGS="-O3 -flto -march=armv8.2-a+dotprod -mtune=cortex-a77 -DNDEBUG" \
+  -DBUILD_SHARED_LIBS=ON \
+  -DGGML_VULKAN=OFF \
+  -DGGML_OPENCL=OFF \
+  -DGGML_DOTPROD=ON \
+  -DGGML_NATIVE=OFF
+
+echo "开始编译"
+sleep 1
+
+ninja
